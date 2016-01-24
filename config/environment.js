b4c6abb90c663fe2172147f603169233d7b757e1
Dilip <dilip@artoo.in>
2016-01-24 18:25:06 +0530

    fix jQuery latest release bug and csp errors.

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,15 @@ module.exports = function(environment) {
       // when it is created
     }
   };
+  
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self'",
+    'font-src': "'self' fonts.gstatic.com",
+    'connect-src': "'self'",
+    'img-src': "'self' data:",
+    'media-src': "'self'"
+  };
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
